5bc6af8d057725a5a04584ad540e6eeb77458898
ginoya <rishitginoya@gmail.com>
2023-11-10 16:51:19 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string to add curreny:");
+
+            string withoutCurrency = Console.ReadLine();
+            Console.WriteLine(withoutCurrency.ToCurrency());
+        }
+    }
+}
